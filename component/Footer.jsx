bc007e1a6b4d8258003ba3e959c16b4d004e8645
Brilliant Makanju <brilliantmakanju4@gmail.com>
2022-12-18 19:21:11 +0100

    Adding the footer done with basic design for footer cont

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className='text-center px-5 py-20 bg-gray-900 text-white w-[100vw]'  >
-            <div className='grid grid-cols-1 md:mr-[-38em] md:ml-[28em]  ' >
-                <input type="email" name="email" className=' rounded-md font-bold md:w-[30%] w-[100%] h-[3em] text-black px-3 ' required />
-                <button className='w-full md:w-[30%] mt-[5px] font-bold h-[3em] rounded-md bg-gray-900 border-[#000000c5] border-2 shadow-none mb-[20px] ' >Join Newsletter</button>
+            <div className=' w-full flex flex-col justify-center items-center  ' >
+                <input type="email" name="email" className=' rounded-md md:w-[40%] font-bold w-[100%] h-[3em] text-black px-3 ' required />
+                <button className='w-full md:w-[40%] mt-[5px] font-bold h-[3em] rounded-md bg-gray-900 border-[#000000c5] border-2 shadow-none mb-[20px] ' >Join Newsletter</button>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-14 ' >
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-14 mt-12 ' >
                 <div className=' mt-[30px] flex md:justify-start md:items-center justify-center items-center md:ml-[5em] ' >
                     <h2>
                         Alkis Savoullis
