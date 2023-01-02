d27a7138a19687eb224cdb30cb6ba72e69c657c8
Charilaos Alkiviades Savoullis <79922848+asavoullis@users.noreply.github.com>
2023-01-02 09:49:43 +0000

    Update data.js
    added links , added new line in overview, changed the order (First Tskills then Sskills , Fixed Percentages, added some of the job descriptions

--- data.js
+++ data.js
@@ -1,91 +1,38 @@
 export const BriefIntro = [
   {
-    name: "Alkiviades Savoullis",
-    quote: "Let's Build the Future",
-    role: "Business Intelligence Developers | MEng Electrical and Electronics Engineering | Entrepreneur | UK-London-Cyprus ",
+    name: "Charilaos Alkiviades Savoullis",
+    quote: "Let's Build the Future Together",
+    role: "Business Intelligence Developer | MEng Electrical and Electronics Engineering | Entrepreneur | UK - Cyprus - Europe",
     overview: " ",
+
     // social: [
-    gitHub: "https://github.com",
-    email: "email.com",
+
+    gitHub: "https://github.com/asavoullis",
+    linkedin: "https://www.linkedin.com",
+    instagram: "https://www.instagram.com",
+    // email: "email.com",
     // 'google.com',
     // "github.com",
     // "email.com",
     // "number",
+
     // ],
   },
 ];
+
 
 export const AboutMe = [
   {
     myImg: "/projects/Alkis2.jpg",
     overview: `
-      I am an ambitious and innovate graduate in Electrical and Electronics Engineering eager to use my Software Engineering, Data Analysis and Business skills through the use of Python, SQL and other similar tools in order to identify and solve complex business problems.
+      I am an ambitious and innovative graduate in Electrical and Electronics Engineering eager to use my Software Engineering, Data Analysis and Business skills through the use of Python, SQL and other similar tools in order to identify and solve complex business problems.
+      
       I am looking to expand and develop my Software Development, Data Processing, Advanced Analytical, Business and Problem Solving skills through the process of manipulating and extracting insights from Big Data to drive future decisions.`,
   },
 ];
 
-// SKills
-
-// Soft Skills:
-// Analytical Skills
-// Consulting
-// Business Analysis
-// Data Analysis
-// Business Development
-// Marketing Analysis
-// Data Management
-// Databases
-// Data Visualisation
-
-export const MySkill = [
-  {
-    id: 1,
-    skillName: "Analytical Skills",
-    skillImg: "/assets/people02.png",
-  },
-  {
-    id: 2,
-    skillName: "Consulting",
-    skillImg: "/assets/people01.png",
-  },
-  {
-    id: 3,
-    skillName: "Business Analysis",
-    skillImg: "/assets/people03.png",
-  },
-  {
-    id: 4,
-    skillName: "Data Analysis",
-    skillImg: "/assets/people01.png",
-  },
-  {
-    id: 5,
-    skillName: "Business Development",
-    skillImg: "/assets/people02.png",
-  },
-  {
-    id: 6,
-    skillName: "Marketing Analysis",
-    skillImg: "/assets/people03.png",
-  },
-  {
-    id: 7,
-    skillName: "Data Management",
-    skillImg: "/assets/people03.png",
-  },
-  {
-    id: 8,
-    skillName: "Databases",
-    skillImg: "/assets/people03.png",
-  },
-  {
-    id: 9,
-    skillName: "Data Visualisation",
-    skillImg: "/assets/people03.png",
-  },
-];
-
 // Technical Skills:
+
 // Python
 // Linux
 // CMD
@@ -95,12 +42,12 @@ export const MySkill = [
 // Snowflake
 // Power BI
 // MS Office
-// Google Analytics
-// DCM
 // PDLC
 // Test Automation
-// Automation
 // Cloud
+// Automation
+// Google Analytics
+// DCM
 // Google Ads
 // SA 360
 
@@ -109,92 +56,241 @@ export const TSkill = [
     id: 1,
     skillName: "Python",
     skillImg: "/assets/people03.png",
+    percentage: 100,
   },
   {
     id: 2,
-    skillName: "Linux",
+    skillName: "Linux - Bash",
     skillImg: "/assets/people03.png",
+    percentage: 100,
   },
   {
     id: 3,
-    skillName: "CMD",
+    skillName: "CMD - Powershell",
     skillImg: "/assets/people03.png",
+    percentage: 100,
   },
   {
     id: 4,
     skillName: "Git",
     skillImg: "/assets/people03.png",
+    percentage: 100,
   },
   {
     id: 5,
     skillName: "SQL",
     skillImg: "/assets/people03.png",
+    percentage: 100,
   },
   {
     id: 6,
     skillName: "AWS",
     skillImg: "/assets/people03.png",
+    percentage: 100,
   },
   {
     id: 7,
     skillName: "Snowflake",
     skillImg: "/assets/people03.png",
+    percentage: 100,
   },
   {
     id: 8,
-    skillName: "Power BI",
+    skillName: "Microsoft Power BI",
     skillImg: "/assets/people03.png",
+    percentage: 100,
   },
   {
     id: 9,
     skillName: "MS Office",
     skillImg: "/assets/people03.png",
+    percentage: 100,
   },
   {
     id: 10,
-    skillName: "Google Analytics",
+    skillName: "PDLC",
     skillImg: "/assets/people03.png",
+    percentage: 100,
   },
   {
     id: 11,
-    skillName: "DCM",
+    skillName: "Test Automation",
     skillImg: "/assets/people03.png",
+    percentage: 100,
   },
   {
     id: 12,
-    skillName: "PDLC",
+    skillName: "Automation",
     skillImg: "/assets/people03.png",
+    percentage: 100,
   },
   {
     id: 13,
-    skillName: "Test Automation",
+    skillName: "Cloud",
     skillImg: "/assets/people03.png",
+    percentage: 90,
   },
   {
     id: 14,
-    skillName: "Automation",
+    skillName: "Google Analytics",
     skillImg: "/assets/people03.png",
+    percentage: 90,
   },
   {
     id: 15,
-    skillName: "Cloud",
+    skillName: "DCM",
     skillImg: "/assets/people03.png",
+    percentage: 90,
   },
   {
     id: 16,
     skillName: "Google Ads",
     skillImg: "/assets/people03.png",
+    percentage: 90,
   },
   {
     id: 17,
     skillName: "SA 360",
     skillImg: "/assets/people03.png",
+    percentage: 90,
+  },
+];
+
+// SKills
+
+// Soft Skills:
+
+// Analytical Skills
+// Consulting
+// Business Analysis
+// Data Analysis
+// Business Development
+// Marketing
+// Data Management
+// Databases
+// Data Visualisation
+// Project Management
+
+export const MySkill = [
+  {
+    id: 1,
+    skillName: "Analytical Skills",
+    skillImg: "/assets/people02.png",
+    percentage: 100,
+  },
+  {
+    id: 2,
+    skillName: "Consulting",
+    skillImg: "/assets/people01.png",
+    percentage: 100,
+  },
+  {
+    id: 3,
+    skillName: "Business Analysis",
+    skillImg: "/assets/people03.png",
+    percentage: 100,
+  },
+  {
+    id: 4,
+    skillName: "Data Analysis",
+    skillImg: "/assets/people01.png",
+    percentage: 100,
+  },
+  {
+    id: 5,
+    skillName: "Business Development",
+    skillImg: "/assets/people02.png",
+    percentage: 100,
+  },
+  {
+    id: 6,
+    skillName: "Marketing",
+    skillImg: "/assets/people03.png",
+    percentage: 100,
+  },
+  {
+    id: 7,
+    skillName: "Data Management",
+    skillImg: "/assets/people03.png",
+    percentage: 100,
+  },
+  {
+    id: 8,
+    skillName: "Databases",
+    skillImg: "/assets/people03.png",
+    percentage: 100,
+  },
+  {
+    id: 9,
+    skillName: "Data Visualisation",
+    skillImg: "/assets/people03.png",
+    percentage: 100,
+  },
+  {
+    id: 10,
+    skillName: "Project Management",
+    skillImg: "/assets/people03.png",
+    percentage: 90,
   },
 ];
 
 export const JobLevel = [
   {
     id: 1,
+    title: "Neo Media World - GroupM",
+    role: "Business Intelligence Developer - Software Developer",
+    location: "London, England, UK",
+    year: "August 2022 - Present",
+    overview: `
+      A crypto conversion app like Coinbase using NextJS, Solidity, thirdweb SDK, Sanity as a headless CMS, authentication and Metamask, and styled-components for styling.`,
+    skills:
+      "Python , SQL , Snowflake , Amazon Web Services (AWS) , Snowflake Cloud , Bash , Powershell , Databases , Data Warehousing , Data Migration , Microsoft Power BI , Business Intelligence , Software Development , Microsoft Office , Google Analytics , Google Ads , DCM , Search Ads 360 , Putty ",
+  },
+  {
+    id: 2,
+    title: "Mediahub UK",
+    role: "Ad Ops Executive",
+    location: "London, England, UK",
+    year: "January 2022 - August 2022",
+    overview: `
+      • Developed and optimised scripts to automate tasks, scrape data from the web, process and visualise data using Python and other tools.
+      • Utilised statistical techniques to interpret data, analyse results and produce comprehensive reports and campaign summaries using analytical platforms such as MOAT, Kochava and Google Analytics.
+      • Evaluated complex data sets in order to identify and interpret trends and patterns. 
+      • Exported data from various platforms to process and analyse using MySQL queries and scripts.
+      • Conducted research to acquire data from both primary and secondary data sources in order to update and maintain databases and data systems.
+      • Setup and management of campaigns in various ad servers such as Google Campaign Manager.
+      • Optimization and troubleshooting technical operations of ad delivery; whilst working with development team to resolve issues. 
+      • Collaborated with Management, Programming and Strategy teams to prioritise business requirements and to locate and define opportunities for campaign improvements.`,
+    skills:
+      "Python , SQL , Amazon Web Services (AWS) , Bash , Powershell , Databases , Data Warehousing , Data Migration , Microsoft Power BI , Microsoft Office , Google Analytics , Google Ads , DCM , Search Ads 360 , Putty ",
+  },
+  {
+    id: 3,
+    title: "Amdocs",
+    role: "Software Engineer in Test",
+    location: "Limassol, Cyprus",
+    year: "October 2021 - January 2022",
+    overview: `
+      • Executed functional/ NF test procedures and scripts either manually or by automated tools. Tracking and reporting on the test execution in a timely manner with attention given to achieving a high level of quality.
+      • Coordinated with Development and Infra teams/peers on the resolution of issues, overseeing the testing progress and ensuring that high standards of quality are being met.
+      • Gathered and documented the outcome of test executions and all information needed to support on going measurements and reporting on risks, defects, tests, and coverage.
+      • Identified, documented and reported bugs, errors, interoperability flaws and other issues within proprietary software applications developed by the Company such as CRM.
+      • Evaluated test exit criteria to assess if additional tests are required or if test scenarios needed to be adjusted.
+      • Participated in and contributed to internal design reviews of testing materials.
+      • Exporting and Importing data to and from various platforms for testing and reporting using MySQL queries and scripts.`,
+    skills: "HTLM",
+  },
+  {
+    id: 4,
+    title: "NewCytech Business Solutions",
+    role: "Network System Engineer Intern",
+    location: "Nicosia, Cyprus",
+    year: "2020-2022",
+    overview:
+      "A crypto conversion app like Coinbase using NextJS, Solidity, thirdweb SDK, Sanity as a headless CMS, authentication and Metamask, and styled-components for styling.",
+  },
+  {
+    id: 5,
     title: "Amazon",
     role: "Frontend Dev",
     year: "2020-2022",
@@ -202,7 +298,7 @@ export const JobLevel = [
       "A crypto conversion app like Coinbase using NextJS, Solidity, thirdweb SDK, Sanity as a headless CMS, authentication and Metamask, and styled-components for styling.",
   },
   {
-    id: 1,
+    id: 6,
     title: "Amazon",
     role: "Frontend Dev",
     year: "2020-2022",
@@ -210,7 +306,7 @@ export const JobLevel = [
       "A crypto conversion app like Coinbase using NextJS, Solidity, thirdweb SDK, Sanity as a headless CMS, authentication and Metamask, and styled-components for styling.",
   },
   {
-    id: 1,
+    id: 7,
     title: "Amazon",
     role: "Frontend Dev",
     year: "2020-2022",
@@ -218,7 +314,7 @@ export const JobLevel = [
       "A crypto conversion app like Coinbase using NextJS, Solidity, thirdweb SDK, Sanity as a headless CMS, authentication and Metamask, and styled-components for styling.",
   },
   {
-    id: 1,
+    id: 8,
     title: "Amazon",
     role: "Frontend Dev",
     year: "2020-2022",
@@ -226,7 +322,7 @@ export const JobLevel = [
       "A crypto conversion app like Coinbase using NextJS, Solidity, thirdweb SDK, Sanity as a headless CMS, authentication and Metamask, and styled-components for styling.",
   },
   {
-    id: 1,
+    id: 9,
     title: "Amazon",
     role: "Frontend Dev",
     year: "2020-2022",
@@ -234,39 +330,7 @@ export const JobLevel = [
       "A crypto conversion app like Coinbase using NextJS, Solidity, thirdweb SDK, Sanity as a headless CMS, authentication and Metamask, and styled-components for styling.",
   },
   {
-    id: 1,
-    title: "Amazon",
-    role: "Frontend Dev",
-    year: "2020-2022",
-    overview:
-      "A crypto conversion app like Coinbase using NextJS, Solidity, thirdweb SDK, Sanity as a headless CMS, authentication and Metamask, and styled-components for styling.",
-  },
-  {
-    id: 1,
-    title: "Amazon",
-    role: "Frontend Dev",
-    year: "2020-2022",
-    overview:
-      "A crypto conversion app like Coinbase using NextJS, Solidity, thirdweb SDK, Sanity as a headless CMS, authentication and Metamask, and styled-components for styling.",
-  },
-  {
-    id: 1,
-    title: "Amazon",
-    role: "Frontend Dev",
-    year: "2020-2022",
-    overview:
-      "A crypto conversion app like Coinbase using NextJS, Solidity, thirdweb SDK, Sanity as a headless CMS, authentication and Metamask, and styled-components for styling.",
-  },
-  {
-    id: 1,
-    title: "Amazon",
-    role: "Frontend Dev",
-    year: "2020-2022",
-    overview:
-      "A crypto conversion app like Coinbase using NextJS, Solidity, thirdweb SDK, Sanity as a headless CMS, authentication and Metamask, and styled-components for styling.",
-  },
-  {
-    id: 1,
+    id: 10,
     title: "Amazon",
     role: "Frontend Dev",
     year: "2020-2022",
